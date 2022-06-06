219d8b0ac5f4f6ad0154b90246643c4415ac8397
isi <isi@751FJW2>
2022-06-06 10:40:58 -0400

    Select solution bspays et pubs

--- mysolutions/select/bd_pays_agregation.sql
+++ mysolutions/select/bd_pays_agregation.sql
@@ -14,3 +14,14 @@ SELECT  COUNT(DISTINCT region) AS "nombre de regions " FROM pays ;
 --6 Quelle est la moyenne de la population des pays d'Europe?
 SELECT CONCAT(AVG(population)/1000000, " Millions") AS "moyenne de la population" FROM pays WHERE region = "Europe";
 --7 Indiquer le nombre de pays dans chacune des régions.
+SELECT region, COUNT(id) FROM pays GROUP BY region;
+--8 Indiquer le nombre de pays dont la population dépasse 50000000 dans chacune des régions
+SELECT region, COUNT(id) as "Nombre de pays" FROM pays  WHERE population>50000000 GROUP BY region ORDER BY  COUNT(id) DESC ;
+--9 Afficher les populations de chacune des régions en millions avec deux chiffres après la virgule.
+SELECT region, FORMAT(SUM(population)/1000000,2) AS "Population"  FROM pays GROUP BY region ORDER BY SUM(population) DESC ;
+--10 Afficher en millions la population totale et le PIB total de l'Europe.
+SELECT CONCAT( FORMAT(SUM(population)/1000000,2) , " M") AS "Population Totale" ,  FORMAT(CONCAT(SUM(pib)/1000000,0), " M") AS "PIB total " FROM pays WHERE region="Europe";
+--11 Afficher en millions la population totale, le PIB total par région et le nom de la région
+SELECT region, CONCAT( FORMAT(SUM(population)/1000000,2) , " M") AS "Population Totale" ,  FORMAT(SUM(pib)/1000000,2) AS "PIB total " FROM pays GROUP BY region ORDER BY SUM(pib) DESC;
+--12 Afficher les régions avec leur population totale, leur superficie et leur produit intérieur brut.
+SELECT region ,  FORMAT(SUM(population),2)   AS "Population" , FORMAT(SUM(superficie),2)  AS "Superficie" ,  SUM(pib) AS "PIB" FROM pays GROUP BY region  ORDER BY SUM(population) DESC;
