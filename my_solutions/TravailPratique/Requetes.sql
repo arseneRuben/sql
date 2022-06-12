6e9c7363a817430b8f37ccfe94962ae31b838b3b
isi <isi@751FJW2>
2022-06-12 16:26:00 -0400

    Contenu des differentes tables

---
+++ my_solutions/TravailPratique/Requetes.sql
@@ -0,0 +1,8 @@
+USE dbBanque;
+/*1 Écrire les requêtes pour afficher le contenu de chacune des tables avec des entêtes significatives pour chaque colonne*/  
+--1-A Liste complete des client
+SELECT * FROM clients;
+--1-B Liste complete des comptes
+SELECT * FROM comptes;
+--1-C Liste complete des comptes
+SELECT * FROM operations;
